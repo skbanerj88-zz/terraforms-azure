ac9f28a62e2c010df18628b8bd155634141f5739
Sourish Kumar Banerjee <13816308+skbanerj88@users.noreply.github.com>
2019-11-21 10:57:33 +0000

    Updated

--- network.tf
+++ network.tf
@@ -8,8 +8,8 @@ resource "azurerm_virtual_network" "VPN" {
   name                = "VirtualNetwork_${local.environment}"
   location            = "${azurerm_resource_group.ResourceGroup.location}"
   resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
-  address_space       = ["${module.variables.address_spaces[0]}"]
-  dns_servers         = ["${module.variables.dns_servers[0]}"]
+  address_space       = "${module.variables.address_spaces[0]}"
+  dns_servers         = "${module.variables.dns_servers[0]}"
 
   subnet {
     name           = "subnet1"
