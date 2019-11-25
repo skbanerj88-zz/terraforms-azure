468dfafd423836dbc7ea3a180c4ffe54efa78427
Sourish Kumar Banerjee <13816308+skbanerj88@users.noreply.github.com>
2019-11-25 16:03:08 +0000

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,32 +1,32 @@
 resource "azurerm_network_security_group" "Security_Group" {
   name                = "SecurityGroup_${local.environment}"
-  location            = "${azurerm_resource_group.ResourceGroup.location}"
-  resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
+  location            = "azurerm_resource_group.ResourceGroup.location"
+  resource_group_name = "azurerm_resource_group.ResourceGroup.name"
 }
 
 resource "azurerm_virtual_network" "VPN" {
   name                = "VirtualNetwork_${local.environment}"
-  location            = "${azurerm_resource_group.ResourceGroup.location}"
-  resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
-  address_space       = "${module.variables.address_spaces[0]}"
-  dns_servers         = "${module.variables.dns_servers[0]}"
+  location            = "azurerm_resource_group.ResourceGroup.location"
+  resource_group_name = "azurerm_resource_group.ResourceGroup.name"
+  address_space       = "module.variables.address_spaces[0]"
+  dns_servers         = "module.variables.dns_servers[0]"
 
   subnet {
     name           = "subnet1"
-    address_prefix = "${tostring(element(split(",", module.variables.addresses[0]),0))}"
-	  security_group = "${azurerm_network_security_group.Security_Group.id}"
+    address_prefix = "tostring(element(split(",", module.variables.addresses[0]),0))"
+	  security_group = "azurerm_network_security_group.Security_Group.id"
   }
 
   subnet {
     name           = "subnet2"
-    address_prefix = "${tostring(element(split(",", module.variables.addresses[0]),1))}"
-	  security_group = "${azurerm_network_security_group.Security_Group.id}"
+    address_prefix = "tostring(element(split(",", module.variables.addresses[0]),1))"
+	  security_group = "azurerm_network_security_group.Security_Group.id"
   }
 
   subnet {
     name           = "subnet3"
-    address_prefix = "${tostring(element(split(",", module.variables.addresses[0]),2))}"
-    security_group = "${azurerm_network_security_group.Security_Group.id}"
+    address_prefix = "tostring(element(split(",", module.variables.addresses[0]),2))"
+    security_group = "azurerm_network_security_group.Security_Group.id"
   }
 
   tags = {
