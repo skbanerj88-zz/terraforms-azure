2856d7fa2070018d52472b170bd7e59649af5285
Sourish Kumar Banerjee <13816308+skbanerj88@users.noreply.github.com>
2019-11-25 16:03:36 +0000

    Update resourceGroup.tf

--- resourceGroup.tf
+++ resourceGroup.tf
@@ -1,5 +1,5 @@
 # Create a resource group
 resource "azurerm_resource_group" "ResourceGroup" {
-  name     = "${local.environment}"
-  location = "${local.location}"
+  name     = "local.environment"
+  location = "local.location"
 }
